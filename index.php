21bebafc6c99fdedae9a4a0ffb8f10ed5be0599c
Husein Febriskamadya <husein.madya@gmail.com>
2016-03-05 08:22:24 +0700

    initial commit
    i become a morning person

---
+++ index.php
@@ -0,0 +1,106 @@
+<html lang="en">
+<head>
+  <!-- Standard Meta -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+
+  <title>Diskon</title>
+
+  <link rel="stylesheet" type="text/css" href="/assets/vendor/semantic-ui/semantic.min.css">
+  <link rel="stylesheet" type="text/css" href="/assets/css/default.css">
+  <script src="/assets/vendor/semantic-ui/semantic.min.js"></script>
+
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+<body>
+
+<header class="dsk header">
+	Diskon
+</header>
+
+<div class="ui four column doubling stackable grid container">
+	<div class="column">
+		
+		<div class="ui link card">
+		  <div class="content">
+		    <div class="right floated meta">s/d 31 Mar</div>
+		    <img class="ui avatar image" src="/assets/content/img/merchant_elv.jpg"> <a href="http://www.elevenia.co.id/">elevenia</a>
+		  </div>
+		  
+		  <a class="image" href="http://www.elevenia.co.id/promo-bonus-hepi-maret-572938"><img src="/assets/content/img/promobanner1.jpg">
+		  </a>
+		  
+		  <div class="content">
+		    <span class="left floated">
+		      <i class="shop icon"></i>
+		      Diskon 7% max. Rp 500rb
+		    </span>
+		  </div>
+		</div>
+		
+	</div>
+	<div class="column">
+		
+		<div class="ui link card">
+		  <div class="content">
+		    <div class="right floated meta">s/d 6 Mar</div>
+		    <img class="ui avatar image" src="/assets/content/img/merchant_lzd.jpg"> Lazada
+		  </div>
+		  <a class="image" href="http://www.lazada.co.id/jelazada-deals/">
+		    <img src="/assets/content/img/promobanner2.jpg">
+		  </a>
+		  <div class="content">
+		    <span class="left floated">
+		      <i class="shop icon"></i>
+		      Diskon 10% max. Rp 200rb
+		    </span>
+		  </div>
+		</div>
+		
+	</div>
+	<div class="column">
+
+		<div class="ui link card">
+		  <div class="content">
+		    <div class="right floated meta">s/d 5 Mar</div>
+		    <img class="ui avatar image" src="/assets/content/img/merchant_bl.jpg"> Bukalapak
+		  </div>
+		  <a class="image" href="https://www.bukalapak.com/promo/promo-baper-edisi-maret">
+		    <img src="/assets/content/img/promobanner3.jpg">
+		  </a>
+		  <div class="content">
+		    <span class="left floated">
+		      <i class="shop icon"></i>
+		      harga mulai rp.1000
+		    </span>
+		  </div>
+		</div>
+
+	</div>
+	<div class="column">
+
+		<div class="ui link card">
+		  <div class="content">
+		    <div class="right floated meta">s/d 6 Mar</div>
+		    <img class="ui avatar image" src="/assets/content/img/merchant_trv.jpg"> Traveloka
+		  </div>
+		  <a class="image" href="http://www.traveloka.com/tgif">
+		    <img src="/assets/content/img/promobanner4.jpg">
+		  </a>
+		  <div class="content">
+		    <span class="left floated">
+		      <i class="shop icon"></i>
+		      Diskon up to Rp 150.000
+		    </span>
+		  </div>
+		</div>
+
+	</div>
+</div>
+
+</body>
+</html>
